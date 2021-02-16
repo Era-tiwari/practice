f99cf9362dd27193bfdec268c00d6ab52de4b8e5
Era-tiwari <74949843+Era-tiwari@users.noreply.github.com>
2021-02-16 20:02:27 +0530

    Pset1
    Input function and numpy module usage.

---
+++ ps1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 16:22:59 2021
+
+@author: Era
+"""
+import numpy
+
+x = int(input('Enter number x:'))
+y = int(input('Enter number y:'))
+print(x**y)
+print(numpy.log2(x))
